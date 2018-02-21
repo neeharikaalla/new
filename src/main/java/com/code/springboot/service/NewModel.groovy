b922238e67fb90a208305a8ddc8a4c211c603c6d
neeharikaalla <36685762+neeharikaalla@users.noreply.github.com>
2018-02-20 21:20:14 -0800

    Add files via upload

---
+++ src/main/java/com/code/springboot/service/NewModel.groovy
@@ -0,0 +1,28 @@
+package com.code.springboot.service
+
+
+/**
+ * Created by yddag on 2/19/2018.
+ */
+
+class NewModel {
+
+    String origin
+    String destination
+
+    String getOrigin() {
+        return origin
+    }
+
+    void setOrigin(String origin) {
+        this.origin = origin
+    }
+
+    String getDestination() {
+        return destination
+    }
+
+    void setDestination(String destination) {
+        this.destination = destination
+    }
+}
