7bee94946105da314008e60f3ec280db585a2b6e
neeharikaalla <36685762+neeharikaalla@users.noreply.github.com>
2018-02-20 21:17:11 -0800

    Create WebApplication.java

---
+++ src/main/java/com/code/springboot/config/WebApplication.java
@@ -0,0 +1,43 @@
+package com.code.springboot.Config;
+
+import com.code.springboot.controller.HelloController;
+import com.code.springboot.service.DistanceService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class WebApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean
+    DistanceService distanceService(){
+        RestTemplate restTemplate = new RestTemplate();
+        return new DistanceService(restTemplate,"https://maps.googleapis.com/maps/api/distancematrix");
+    }
+
+    @Bean
+    HelloController helloController(){
+        return new HelloController(distanceService());
+    }
+}
+
